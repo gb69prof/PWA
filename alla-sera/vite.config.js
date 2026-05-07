5d10fad29563825fee1ef8ae110bb878aa311a34
gb69prof <gb69prof@users.noreply.github.com>
2026-05-07 09:01:07 +0200

    Delete alla-sera directory

--- alla-sera/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  // Se cambi nome al repository, cambia anche questa riga.
-  // Esempio: repository "Foscolo" => base: "/Foscolo/"
-  base: "/alla-sera-immersiva/"
-});
